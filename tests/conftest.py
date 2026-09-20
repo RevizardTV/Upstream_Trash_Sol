c8ee5a0511b4e1eb04b9504c32eb90bc3eb8c777
RevizardTV <168398688+RevizardTV@users.noreply.github.com>
2026-09-20 14:31:41 +0530

    environ testing 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ os.environ.setdefault("DB_USER", "test_user")
 os.environ.setdefault("DB_PASSWORD", "test_password")
 os.environ.setdefault("DB_NAME", "test_db")
 os.environ.setdefault("DB_HOST", "localhost")
-os.environ.setdefault("DB_PORT", "5432")
+os.environ.setdefault("DB_PORT", "3306")
+os.environ.setdefault("REDIS_URL", "redis://localhost:6379")
+os.environ.setdefault("RESEND_API_KEY", "re_dummy_key_for_testing")
 
 import sys
 
